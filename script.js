284391a37a3499443a28a7c80e03b7752e6729f6
caiohenrqq <caiohenrqq@gmail.com>
2023-06-12 13:03:08 -0400

    feature: javascript, display arrow

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function scrollToTop() {
     window.scrollTo(0, 0);
 }
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 400) {
+      const elements = document.getElementsByClassName("scrollTop");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+      }
+    } else {
+      const elements = document.getElementsByClassName("scrollTop");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "inline";
+      }
+    }
+  });
+  
+  
